8978927226bb448afabea2a3b2d841975a25f398
esayas82 <esayasretta1@gmail.com>
2025-05-10 22:39:41 -0400

    updated Axios and Requests

---
+++ netflix-clone-2025/src/components/Banner/Banner.jsx
@@ -0,0 +1,58 @@
+
+import React, { useState, useEffect } from "react";
+import "./Banner.css";
+import Axios from '../../utils/Axios'
+import Requests from '../../utils/Requests'
+
+const Banner = () => {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await Axios.get(Requests.fetchNetflixOriginals);
+        console.log(request);
+        setMovie(
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ]
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    })();
+  }, []);
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </div>
+  );
+};
+
+export default Banner;
+
--- netflix-clone-2025/src/utils/Requests.js
+++ netflix-clone-2025/src/utils/Requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
@@ -10,3 +10,5 @@ const requests = {
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
     fetchTvShows: `/discover/tv?api_key=${API_KEY}&language=en-US&page=1`
 }
+
+export default requests;
